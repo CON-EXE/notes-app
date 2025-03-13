83340a7d3d84453457549b42c7f10d4344c27ff0
Connor Hall <connrhall204@gmail.com>
2025-03-13 18:53:12 -0500

    Project 'complete'

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,20 @@
 'use strict';
 
-const { log } = console;
-
 let title = '';
 let content = '';
-let count = 0;
 const list = [];
 
 const newNote = document.querySelector('.new-note');
+const save = document.querySelector('.save');
 const cancel = document.querySelector('.cancel');
 const popup = document.querySelector('.overlay');
 const form = document.querySelector('form');
+const noteList = document.querySelector('ul');
+const titleInput = document.querySelector('.title');
+const contentInput = document.querySelector('.content');
+const titleDisplay = document.querySelector('.display-title');
+const contentDisplay = document.querySelector('.display-content');
+const dateDisplay = document.querySelector('.display-date');
 
 class Note {
     #title;
@@ -25,7 +29,7 @@ class Note {
 
     getDate() {
         const now = new Date();
-        return `${now.getMonth() + 1}/${now.getDate()}/${now.getFullYear}`;
+        return `${now.getMonth() + 1}/${now.getDate()}/${now.getFullYear()}`;
     }
 
     get title() {
@@ -42,18 +46,66 @@ class Note {
 }
 
 function createNote() {
-    const savedNote = document.createElement('div');
-    savedNote.setAttribute('data-index', count);
-    savedNote.setAttribute('onclick', 'displayNote(this)');
-    noteList.append(savedNote);
     const note = new Note(titleInput.value, contentInput.value);
-    list.push(note);
+    list.unshift(note);
+    loadNotes();
+}
+
+function loadNotes() {
+    if (list.length > 0) {
+        noteList.innerHTML = '';
+        for (let i = 0; i < list.length; i++) {
+            let listNote = document.createElement('li');
+            listNote.setAttribute('data-index', i);
+            listNote.setAttribute('onclick', 'displayNote(this)');
+            listNote.innerHTML += `
+                            <li class=flex>
+                                ${list[i].title}
+                                <div class="note-buttons">
+                                    <a class="edit">
+                                        <i class="fa-solid fa-pencil"></i>
+                                    </a>
+                                    <a class="delete">
+                                        <i class="fa-solid fa-trash"></i>
+                                    </a>
+                                </div>
+                            </li>`;
+            noteList.append(listNote);
+        }
+
+        titleDisplay.innerText = list[0].title;
+        contentDisplay.innerText = list[0].content;
+        dateDisplay.innerText = `Last save: ${list[0].date}`;
+    }
+}
+
+function displayNote(note) {
+    let index = parseInt(note.dataset.index);
+    titleDisplay.innerText = list[index].title;
+    contentDisplay.innerText = list[index].content;
+    dateDisplay.innerText = `Last save: ${list[index].date}`;
+}
+
+function clearInput() {
+    titleInput.value = '';
+    contentInput.value = '';
 }
 
 newNote.addEventListener('click', () => {
     popup.classList.remove('hide');
 });
 
+save.addEventListener('click', () => {
+    popup.classList.add('hide');
+    createNote();
+    clearInput();
+});
+
 cancel.addEventListener('click', () => {
     popup.classList.add('hide');
-})
+    clearInput();
+});
+
+window.addEventListener('load', () => {
+    loadNotes();
+});
